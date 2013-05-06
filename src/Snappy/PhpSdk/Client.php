e3ccbb7408b5d6e974c799341ed6d554cbe41321
Taylor Otwell <taylorotwell@gmail.com>
2013-05-06 10:40:52 -0500

    Working on SDK.

--- src/Snappy/PhpSdk/Client.php
+++ src/Snappy/PhpSdk/Client.php
@@ -40,6 +40,140 @@ class Client {
 	}
 
 	/**
+	 * Get all of the mailboxes for a given account ID.
+	 *
+	 * @param  int  $accountId
+	 * @return array
+	 */
+	public function getMailboxes($accountId)
+	{
+		return $this->send($this->getHttp()->get(static::URL.'account/'.$accountId.'/mailboxes'));
+	}
+
+	/**
+	 * Get all of the documents for a given account ID.
+	 *
+	 * @param  int  $accountId
+	 * @return array
+	 */
+	public function getDocuments($accountId)
+	{
+		return $this->send($this->getHttp()->get(static::URL.'account/'.$accountId.'/documents'));	
+	}
+
+	/**
+	 * Get all of the staff for a given account ID.
+	 *
+	 * @param  int  $accountId
+	 * @return array
+	 */
+	public function getStaff($accountId)
+	{
+		return $this->send($this->getHttp()->get(static::URL.'account/'.$accountId.'/staff'));
+	}
+
+	/**
+	 * Get all of the tickets for a given mailbox ID.
+	 *
+	 * @param  int  $mailboxId
+	 * @return array
+	 */
+	public function getWaitingTickets($mailboxId)
+	{
+		return $this->send($this->getHttp()->get(static::URL.'mailbox/'.$mailboxId.'/tickets'));
+	}
+
+	/**
+	 * Get the details for a given ticket ID.
+	 *
+	 * @param  int  $ticketId
+	 * @return array
+	 */
+	public function getTicketDetails($ticketId)
+	{
+		return $this->send($this->getHttp()->get(static::URL.'ticket/'.$ticketId));
+	}
+
+	/**
+	 * Get the notes for the given ticket ID.
+	 *
+	 * @param  int  $ticketId
+	 * @return array
+	 */
+	public function getTicketNotes($ticketId)
+	{
+		return $this->send($this->getHttp()->get(static::URL.'ticket/'.$ticketId.'/notes'));	
+	}
+
+	/**
+	 * Send a new message via your Snappy account.
+	 *
+	 * @param  \Snappy\PhpSdk\Message  $message
+	 * @return void
+	 */
+	public function sendMessage(Message $message)
+	{
+		$request = $this->getHttp()->post(static::URL.'note', null, $this->buildMessagePayload($message));
+
+		$this->sendPlain($request);
+	}
+
+	/**
+	 * Build the JSON payload for sending a note to the API.
+	 *
+	 * @param  \Snappy\PhpSdk\Message  $message
+	 * @return string
+	 */
+	protected function buildMessagePayload(Message $message)
+	{
+		$payload = array();
+
+		if (isset($message->mailboxId)) {
+			$payload['mailbox_id'] = $message->mailboxId;
+		}
+
+		if (isset($note->staffId)) {
+			$payload['staff_id'] = $note->staffId;
+		}
+
+		$payload['id'] = $note->ticketId;
+		$payload['subject'] = (string) $note->subject;
+		$payload['message'] = $note->message;
+		$payload['attachments'] = array();
+		$payload['tags'] = $note->tags;
+		$payload['scope'] = $note->scope ?: 'public';
+
+		if (isset($message->from)) {
+			$payload['from'][] = array('name' => $message->from['name'], 'address' => $message->from['address']);			
+		}
+
+		foreach (array('to', 'bcc', 'cc') as $type) {
+			foreach ($message->{$type} as $name => $address) {
+				if (is_numeric($name)) $name = $address;
+				$payload[$type][] = compact('name', 'address');
+			}
+		}
+
+		return json_encode($payload);
+	}
+
+	/**
+	 * Replace a given ticket's tags with the given tags.
+	 *
+	 * @param  int    $ticketId
+	 * @param  array  $tags
+	 * @return void
+	 */
+	public function updateTicketTags($ticketId, array $tags)
+	{
+		$request = $this->getHttp()->post(static::URL.'ticket/'.$ticketId.'/tags');
+
+		$request->addPostFields(array('tags' => json_encode($tags)));
+
+		$this->sendPlain($request);
+	}
+
+	/**
 	 * Send the request and return the JSON payload as an array.
 	 *
 	 * @param  \Guzzle\Http\Message\Request  $request
@@ -53,6 +187,17 @@ class Client {
 		$response = $request->send();
 
 		return $json ? json_decode($response->getBody(), true) : $response->getBody();
+	}
+
+	/**
+	 * Send the request and get a plain response.
+	 *
+	 * @param  \Guzzle\Http\Message\Request  $request
+	 * @return string
+	 */
+	protected function sendPlain(Request $request)
+	{
+		return $this->send($request, false);
 	}
 
 	/**
---
+++ src/Snappy/PhpSdk/Message.php
@@ -0,0 +1,151 @@
+<?php namespace Snappy\PhpSdk;
+
+class Message {
+
+	/**
+	 * The mailbox ID of the message the mailbox is assigned to.
+	 *
+	 * @var int
+	 */
+	public $mailboxId;
+
+	/**
+	 * The ticket ID (if any) the message should be attached to.
+	 *
+	 * @var int
+	 */
+	public $ticketId;
+
+	/**
+	 * The staff ID of the person sending the message (if outgoing).
+	 *
+	 * @var int
+	 */
+	public $staffId;
+
+	/**
+	 * The scope of the message (public or private)
+	 *
+	 * @var string
+	 */
+	public $scope;
+
+	/**
+	 * The subject of the message.
+	 *
+	 * @var string
+	 */
+	public $subject;
+
+	/**
+	 * The sender of the message (if incoming).
+	 *
+	 * @var array
+	 */
+	public $from;
+
+	/**
+	 * The recipients of the message.
+	 *
+	 * @var array
+	 */
+	public $to = array();
+
+	/**
+	 * The copied recipients of the message.
+	 *
+	 * @var array
+	 */
+	public $cc = array();
+
+	/**
+	 * The blind copied recipients of the message.
+	 *
+	 * @var array
+	 */
+	public $bcc = array();
+
+	/**
+	 * The contents of the message.
+	 *
+	 * @var string
+	 */
+	public $message;
+
+	/**
+	 * The tags to attach to the ticket (if outgoing)
+	 *
+	 * @var array
+	 */
+	public $tags = array();
+
+	/**
+	 * Set the sender of the message.
+	 *
+	 * @param  string  $name
+	 * @param  string  $address
+	 * @return \Snappy\PhpSdk\Message
+	 */
+	public function setFrom($address, $name = null)
+	{
+		if (is_null($name)) $name = $address;
+
+		$this->from = compact('name', 'address');
+
+		return $this;
+	}
+
+	/**
+	 * Add a recipient to the message.
+	 *
+	 * @param  string  $address
+	 * @param  string  $name
+	 * @return \Snappy\PhpSdk\Message
+	 */
+	public function addTo($address, $name = null)
+	{
+		return $this->addRecipient('to', $address, $name);
+	}
+
+	/**
+	 * Add a copied recipient to the message.
+	 *
+	 * @param  string  $address
+	 * @param  string  $name
+	 * @return \Snappy\PhpSdk\Message
+	 */
+	public function addCc($address, $name = null)
+	{
+		return $this->addRecipient('cc', $address, $name);
+	}
+
+	/**
+	 * Add a blind copied recipient to the message.
+	 *
+	 * @param  string  $address
+	 * @param  string  $name
+	 * @return \Snappy\PhpSdk\Message
+	 */
+	public function addBcc($address, $name = null)
+	{
+		return $this->addRecipient('bcc', $address, $name);
+	}
+
+	/**
+	 * Add a recipient to the messgae.
+	 *
+	 * @param  string  $type
+	 * @param  string  $address
+	 * @param  string  $name
+	 * @return \Snappy\PhpSdk\Message
+	 */
+	protected function addRecipient($type, $address, $name)
+	{
+		if (is_null($name)) $name = $address;
+
+		$this->{$type}[] = compact('name', 'address');
+
+		return $this;
+	}
+
+}
